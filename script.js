566f51f01c31a6d8b55faf035c921d7a3875765f
HenryMikitish <henry.mikitish@gmail.com>
2023-02-01 10:31:20 -0800

    Debug js and add a flash to each output onscreen

--- script.js
+++ script.js
@@ -20,13 +20,35 @@ const divide = document.querySelector('#divide');
 const multiply = document.querySelector('#multiply');
 const minus = document.querySelector('#minus');
 const plus = document.querySelector('#plus');
+const output = document.querySelector('#output');
 
 let numberOne = '';
 let numberTwo = '';
 let operator ='';
 output.textContent = '';
+let lastInput = null;
 
 function nextAction(e) {
+
+    //ensures no division by 0
+    if ((e.target.textContent == '÷' || 
+        e.target.textContent == 'x' || 
+        e.target.textContent == '-' || 
+        e.target.textContent == '+' || 
+        e.target.textContent == '=') 
+        && 
+        lastInput == '0' && operator == 'divide') {
+        output.textContent = 'NICE TRY';
+        return;
+    }
+
+    if(output.textContent == 'NICE TRY') {
+        numberOne = '';
+        numberTwo = '';
+        operator ='';
+        output.textContent = '';
+        return;
+    }
 
     //this section clears board, aborts the calculation, and leaves the func.
     if (e.target == clear) {
@@ -37,6 +59,7 @@ function nextAction(e) {
         return;
     }
 
+    //this section handles pressing a number after =
     if (operator == 'equals' && !e.target.hasAttribute('id')) {
         numberOne = '';
         numberTwo = '';
@@ -47,68 +70,99 @@ function nextAction(e) {
         return;
     }
 
-    //if (output.textContent.length < 10) {
-    
-        //this section handles the second number and prints the result to numberOne
+    //this section handles pressing operators in succession
+    if ((e.target.textContent == '÷' || 
+        e.target.textContent == 'x' || 
+        e.target.textContent == '-' || 
+        e.target.textContent == '+' || 
+        e.target.textContent == '=') 
+        && 
+        ((lastInput == '÷') || 
+        (lastInput == 'x') || 
+        (lastInput == '-') || 
+        (lastInput == '+')
+        )
+        ||
+        (e.target.textContent == '=' && lastInput == '='))
+        {
+
         if (operator == 'divide' || operator == 'multiply' || operator == 'minus' || operator == 'plus') {
-            
-            if (operator == 'divide' && 
-            e.target.hasAttribute('id')) {
-                let result = Number(numberOne) / Number(numberTwo);
-                numberTwo = '';
-                operator = e.target.getAttribute('id');
-                numberOne = result;
-                }
-            
-            else if (operator == 'multiply' && 
-            e.target.hasAttribute('id')) {
-                let result = Number(numberOne) * Number(numberTwo);
-                numberTwo = '';
-                operator = e.target.getAttribute('id');
-                numberOne = result;
-                }   
-
-            else if (operator == 'minus' && 
-            e.target.hasAttribute('id')) {
-                let result = Number(numberOne) - Number(numberTwo);
-                numberTwo = '';
-                operator = e.target.getAttribute('id');
-                numberOne = result;
-                }  
-            
-            else if (operator == 'plus' && 
-            e.target.hasAttribute('id')) {
-                let result = Number(numberOne) + Number(numberTwo);
-                numberTwo = '';
-                operator = e.target.getAttribute('id');
-                numberOne = result;
-                } 
-
-            else {numberTwo += e.target.textContent}
+        operator = e.target.getAttribute('id');
         }
+
+        return;
+    }
+    
+    //this section handles the second number and prints the result to numberOne
+    if (operator == 'divide' || operator == 'multiply' || operator == 'minus' || operator == 'plus') {
         
-        //this section factors the first number
-        else if (e.target == divide) {operator = 'divide'}
-        else if (e.target == multiply) {operator = 'multiply'}
-        else if (e.target == minus) {operator = 'minus'}
-        else if (e.target == plus) {operator = 'plus'}
-        else if (e.target.textContent == '=') {return}
+        if (operator == 'divide' && 
+        e.target.hasAttribute('id')) {
+            let result = Number(numberOne) / Number(numberTwo);
+            numberTwo = '';
+            operator = e.target.getAttribute('id');
+            numberOne = result;
+            }
+        
+        else if (operator == 'multiply' && 
+        e.target.hasAttribute('id')) {
+            let result = Number(numberOne) * Number(numberTwo);
+            numberTwo = '';
+            operator = e.target.getAttribute('id');
+            numberOne = result;
+            }   
 
-        else {numberOne += e.target.textContent}
+        else if (operator == 'minus' && 
+        e.target.hasAttribute('id')) {
+            let result = Number(numberOne) - Number(numberTwo);
+            numberTwo = '';
+            operator = e.target.getAttribute('id');
+            numberOne = result;
+            }  
+        
+        else if (operator == 'plus' && 
+        e.target.hasAttribute('id')) {
+            let result = Number(numberOne) + Number(numberTwo);
+            numberTwo = '';
+            operator = e.target.getAttribute('id');
+            numberOne = result;
+            } 
 
-        //this section determines what is displayed on-screen
-        if (numberTwo > 0) {
-            output.textContent = numberTwo;
-        }
-        else {output.textContent = numberOne}
+        else {numberTwo += e.target.textContent}
+    }
+        
+    //this section factors the first number
+    else if (e.target == divide) {operator = 'divide'}
+    else if (e.target == multiply) {operator = 'multiply'}
+    else if (e.target == minus) {operator = 'minus'}
+    else if (e.target == plus) {operator = 'plus'}
+    else if (e.target.textContent == '=') {return}
+    else if (output.textContent.includes('.') && e.target.textContent == '.') {return}
 
-    //}
+    else {numberOne += e.target.textContent}
 
+    //this section determines what is displayed on-screen
+    if (numberTwo >= 0 && 
+        ((lastInput == '÷') || 
+        (lastInput == 'x') || 
+        (lastInput == '-') || 
+        (lastInput == '+')
+        )) {
+        output.textContent = numberTwo;
+    }
+    else {output.textContent = numberOne}
+}
+
+//also creates an animation
+function logAction(e) {
+    output.classList.add('output-flash');
+    setTimeout(() => {output.classList.remove('output-flash')}, 200);
+    lastInput = e.target.textContent;
 }
 
 
 actions.forEach(action => action.addEventListener('click', nextAction));
-//equals.addEventListener('click', solve);
+actions.forEach(action => action.addEventListener('click', logAction));
 
 
 
